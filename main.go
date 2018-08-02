98fe36ad8711ba212300c4673ead738c92b3158c
nicoalimin <nicoalimin@yahoo.com>
2018-08-01 22:53:35 -0700

    added sub main.go file

---
+++ main.go
@@ -0,0 +1,16 @@
+/*
+ * Filename: c:\CPSC\github\teamgenerator\server\main.go
+ * Path: c:\CPSC\github\teamgenerator\server
+ * Created Date: Wednesday, August 1st 2018, 9:16:51 pm
+ * Author: Nico
+ * 
+ * Copyright (c) 2018 Your Company
+ */
+
+package main
+
+import fmt
+
+func main() {
+	fmt.Println("Welcome to go")
+}
